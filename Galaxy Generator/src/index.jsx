af4a21df840ee077b5d6957a150b2a508ab0e815
sudoneoox <76002926+sudoneoox@users.noreply.github.com>
2021-06-14 07:24:00 -0500

    added Blackhole file and shader still need to debug it and add a debug panel

--- Galaxy Generator/src/index.jsx
+++ Galaxy Generator/src/index.jsx
@@ -6,7 +6,6 @@ import App from './jsx/App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-
   </React.StrictMode>,
   document.getElementById('root')
 );
--- Galaxy Generator/src/jsx/App.jsx
+++ Galaxy Generator/src/jsx/App.jsx
@@ -1,139 +1,48 @@
-import React, { useRef, useMemo } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import './ShaderMaterial/StarsMaterial';
-import * as THREE from 'three';
+import { EffectComposer, Bloom } from '@react-three/postprocessing';
+import Stars from './ShaderMaterial/Stars/Stars';
+import Blackhole from './ShaderMaterial/blackhole/BlackHole';
+import Observer from './ShaderMaterial/Camera/Observer';
 
-function Stars({ amount }) {
 
-    // PARAMETERS FOR STARS
-    const parameters = {};
-    parameters.count = 2000000;
-    parameters.size = .05;
-    parameters.radius = 5;
-    parameters.branches = 3;
-    parameters.spin = 1;
-    parameters.randomness = 0.5;
-    parameters.randomnessPower = 3;
-    parameters.insideColor = "#e54ed0";
-    parameters.outsideColor = "#00076f";
-
-    const insideColor = new THREE.Color(parameters.insideColor);
-    const outsideColor = new THREE.Color(parameters.outsideColor);
-
-    const [positions, colors, randomness, scales] = useMemo(() => {
-        let positions = [], colors = [], randomness = [], scales = [];
-        // Randomize the XYZ Coords and Color for each Point
-        for (let i = 0; i < amount; i++) {
-            const radius = Math.random() * parameters.radius;
-
-            const branchAngle =
-                ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
-
-            const randomX =
-                Math.pow(Math.random(), parameters.randomnessPower) *
-                (Math.random() < 0.5 ? 1 : -1) *
-                parameters.randomness *
-                radius;
-
-            const randomY =
-                Math.pow(Math.random(), parameters.randomnessPower) *
-                (Math.random() < 0.5 ? 1 : -1) *
-                parameters.randomness *
-                radius;
-
-            const randomZ =
-                Math.pow(Math.random(), parameters.randomnessPower) *
-                (Math.random() < 0.5 ? 1 : -1) *
-                parameters.randomness *
-                radius;
-
-            positions.push(Math.cos(branchAngle) * radius);
-            positions.push(0);
-            positions.push(Math.sin(branchAngle) * radius);
-
-            randomness.push(randomX);
-            randomness.push(randomY);
-            randomness.push(randomZ);
-
-            const mixedColor = insideColor.clone();
-            mixedColor.lerp(outsideColor, radius / parameters.radius);
-
-            // colors.push(mixedColor.r);
-            // colors.push(mixedColor.g);
-            // colors.push(mixedColor.b);
-
-            colors[i * 3] = mixedColor.r;
-            colors[i * 3 + 1] = mixedColor.g;
-            colors[i * 3 + 2] = mixedColor.b;
-
-            scales.push(Math.random());
-        }
-        return [
-            new Float32Array(positions),
-            new Float32Array(colors),
-            new Float32Array(randomness),
-            new Float32Array(scales)];
-
-    }, [amount]);
-
-    const attribute = useRef();
-
-    useFrame((state) => (attribute.current.uniforms.uTime.value = state.clock.elapsedTime));
-
+function Scene() {
     return (
-        <points>
-            <bufferGeometry attach="geometry">
-
-                {/* POSITION ATTRIBUTE */}
-                <bufferAttribute
-                    attachObject={["attributes", "position"]}
-                    count={positions.length / 3}
-                    array={positions}
-                    itemSize={3} />
-                {/* COLOR ATTRIBUTE */}
-                <bufferAttribute ref={attribute}
-                    attachObject={["attributes", "color"]}
-                    count={colors.length / 3}
-                    array={colors}
-                    itemSize={3} />
-                {/* RANDOM ATTRIBUTE */}
-                <bufferAttribute
-                    attachObject={["attributes", "aRandomness"]}
-                    count={randomness.length / 3}
-                    array={randomness}
-                    itemSize={3} />
-                <bufferAttribute
-                    attachObject={["attributes", "aScale"]}
-                    count={scales.length}
-                    array={scales}
-                    itemSize={1}
-                />
-
-            </bufferGeometry>
-
-            {/* TODO: UPDATE MATERIAL TO USE SHADER MATERIAL */}
-            <starsMaterial ref={attribute} attach="material" />
-
-        </points>
-    );
-}
-
-
-export default function App() {
-    return (
-        <Canvas colorManagement
-            camera={{ position: [3, 3, 3], fov: 75 }}
-            gl={{ powerPreference: 'high-performance', antialias: true, pixelRatio: Math.min(window.devicePixelRatio) }}
-            dpr={Math.min(window.devicePixelRatio, 2)}>
-
+        <>
             <Stars amount={200000} />
-
+            <Blackhole />
             <OrbitControls
                 dampingFactor={.05}
                 enablePan={true}
                 enableDamping={true}
             />
+            <EffectComposer smaa>
+                <Bloom
+                    intensity={.8}
+                    luminanceThreshold={0}
+                />
+            </EffectComposer>
+        </>
+    );
+}
+
+
+export default function App() {
+
+    const observer = new Observer(60, window.innerWidth / window.innerHeight, 1, 80000);
+    observer.distance = 8;
+    return (
+        <Canvas colorManagement
+            gl={{
+                powerPreference: 'high-performance',
+                antialias: true,
+                pixelRatio: Math.min(window.devicePixelRatio)
+            }}
+            dpr={Math.min(window.devicePixelRatio, 2)}>
+            <Suspense fallback={null}>
+                <Scene />
+            </Suspense>
         </Canvas>
     );
 }
